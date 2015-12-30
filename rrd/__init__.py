480e4e190327106824eba29f819950f3036551a4
root <root@mywork.(none)>
2015-12-30 17:56:55 +0800

    dashboard  Permission Validation

--- rrd/__init__.py
+++ rrd/__init__.py
@@ -1,14 +1,51 @@
 #-*- coding:utf-8 -*-
 import os
-from flask import Flask
-
+from flask import Flask, request, g, session, make_response, redirect
+from rrd.api import uic
+import logging
 #-- create app --
 app = Flask(__name__)
 app.config.from_object("rrd.config")
+
+IGNORE_PREFIX = ['/api', '/static']
+# config log
+log_formatter = '%(asctime)s\t[%(filename)s:%(lineno)d] [%(levelname)s: %(message)s]'
+log_level = logging.DEBUG if app.config['DEBUG'] else logging.WARNING
+logging.basicConfig(format=log_formatter, datefmt="%Y-%m-%d %H:%M:%S", level=log_level)
 
 @app.errorhandler(Exception)
 def all_exception_handler(error):
     print "exception: %s" %error
     return u'dashboard 暂时无法访问，请联系管理员', 500
+
+
+@app.before_request
+def before_request():
+    p = request.path
+    for ignore_pre in IGNORE_PREFIX:
+        if p.startswith(ignore_pre):
+            return
+    
+    if 'user_name' in session and session['user_name']:
+        g.user_name = session['user_name']
+    else:
+        sig = request.cookies.get('sig')
+        if not sig:
+            return redirect_to_sso()
+
+        username = uic.username_from_sso(sig)
+        if not username:
+            return redirect_to_sso()
+
+        session['user_name'] = username
+        g.user_name = session['user_name']
+
+	
+def redirect_to_sso():
+    logging.debug('sso')
+    sig = uic.gen_sig()
+    resp = make_response(redirect(uic.login_url(sig, urllib.quote(request.url))))
+    resp.set_cookie('sig', sig)
+    return resp
 
 from view import api, chart, screen, index
--- rrd/config.py
+++ rrd/config.py
@@ -1,6 +1,18 @@
 #-*-coding:utf8-*-
 import os
 
+
+# -- cookie config --
+SECRET_KEY = "4e.5tyg8-u9ioj"
+SESSION_COOKIE_NAME = "falcon-dashbord"
+PERMANENT_SESSION_LIFETIME = 3600 * 24 * 30
+#--uic config--
+UIC_ADDRESS = {
+    'internal': 'http://127.0.0.1:1234',
+    'external': 'http://192.168.136.131:1234',
+}
+
+UIC_TOKEN = ''
 #-- dashboard db config --
 DASHBOARD_DB_HOST = "127.0.0.1"
 DASHBOARD_DB_PORT = 3306
@@ -18,14 +30,14 @@ GRAPH_DB_NAME = "graph"
 #-- app config --
 DEBUG = True
 SECRET_KEY = "secret-key"
-SESSION_COOKIE_NAME = "open-falcon"
+SESSION_COOKIE_NAME = "falcon_dashbord"
 PERMANENT_SESSION_LIFETIME = 3600 * 24 * 30
 SITE_COOKIE = "open-falcon-ck"
 
 #-- query config --
 QUERY_ADDR = "http://127.0.0.1:9966"
 
-BASE_DIR = "/home/work/open-falcon/dashboard/"
+BASE_DIR = "/root/work/github.com/open-falcon/dashboard/"
 LOG_PATH = os.path.join(BASE_DIR,"log/")
 
 try:
